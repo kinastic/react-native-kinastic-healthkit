2e6efbe6d0b21f5b87bdb58c2c9c7b952225e80f
Jose Rota <jose@kinastic.com>
2020-03-06 01:14:15 +0100

    minor fix

--- src/HKObjectType.ts
+++ src/HKObjectType.ts
@@ -1,7 +1,7 @@
 import { HKCharacteristicType, HKCharacteristicTypes } from './HKCharacteristicType';
 import { HKSampleType, HKSampleTypes } from './HKSampleType';
 
-export type HKObjectType = HKSampleType & HKCharacteristicType;
+export type HKObjectType = HKSampleType | HKCharacteristicType;
 
 const HKObjectTypes: string[] = [...HKCharacteristicTypes, ...HKSampleTypes];
 
